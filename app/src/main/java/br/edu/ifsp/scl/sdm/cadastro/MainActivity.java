20807cbeb7c8e933c9f61dbef16a2c46a6fc9f50
DFSISDELLI <sisdellidafne@gmail.com>
2021-10-12 17:50:52 -0300

    cadastro

--- app/src/main/java/br/edu/ifsp/scl/sdm/cadastro/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/sdm/cadastro/MainActivity.java
@@ -1,14 +1,54 @@
 package br.edu.ifsp.scl.sdm.cadastro;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+{
 
-public class MainActivity extends AppCompatActivity {
+    private EditText nomeCompletoEt;
+    private EditText telefoneEt;
+    private EditText emailEt;
+    private CheckBox ingressarCb;
+    private RadioGroup sexoRg;
+    private RadioButton masculinoRb;
+    private RadioButton femininoRb;
+    private EditText cidadeEt;
+    private Spinner ufSp;
+    private Button limparBt;
+    private Button salvarBt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    nomeCompletoEt = findViewById(R.id.nomeCompletoEt);
+    telefoneEt = findViewById(R.id.telefoneEt) ;
+    emailEt = findViewById(R.id.emailEt);
+    ingressarCb = findViewById(R.id.ingressarCb);
+    sexoRg = findViewById(R.id.sexoRg);
+    masculinoRb = findViewById(R.id.masculinoRb);
+    femininoRb = findViewById(R.id.femininoRb);
+    cidadeEt = findViewById(R.id.cidadeEt);
+    ufSp = findViewById(R.id.ufSp);
+    limparBt = findViewById(R.id.limparBt);
+    salvarBt = findViewById(R.id.salvarBt);
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
